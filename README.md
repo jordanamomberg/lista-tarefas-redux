512038ea5f63fb52a32e8a1f20cb803412e04d2f
Jordana Momberg <jordanamomberg@gmail,com>
2020-08-16 17:21:24 -0300

    readme

---
+++ README.md
@@ -0,0 +1,29 @@
+
+
+# Lista de tarefas
+
+Implementado um CRUD e utilizando Redux para gerenciamento de Estado.
+
+### Libs 
+
+- React
+- React-router-dom
+- React-toastify
+- Axios
+- React-icons
+- Material-ui/core
+- React-redux
+- Redux
+
+### Start
+
+``` 
+- Instalar as dependências 
+yarn install
+
+- Iniciar
+yarn dev 
+
+```
+
+![Tarefas](https://github.com/jordanamomberg/lista-tarefas/blob/master/tarefas.png?raw=true)
--- src/pages/Tasks/index.js
+++ src/pages/Tasks/index.js
@@ -36,8 +36,6 @@ import {
 import { Brightness4, Brightness7, Home, Info } from "@material-ui/icons";
 import { Tooltip } from "@material-ui/core";
 
-import TodoChips from "../../components/todo-chips";
-
 import { Box, Chip } from "@material-ui/core";
 import { pink } from "@material-ui/core/colors";
 import todos from "../../Store/reducers/todos";
@@ -97,7 +95,7 @@ function Tasks(props) {
           >
             <FiSearch size={24} color="#FF1493" />
           </a>
-          <AppBar position="fixed">
+          {/* <AppBar position="fixed">
             <Toolbar>
               <IconButton color="inherit" aria-label="open drawer" edge="start">
                 <MenuIcon />
@@ -121,7 +119,7 @@ function Tasks(props) {
                 </IconButton>
               </div>
             </Toolbar>
-          </AppBar>
+          </AppBar> */}
         </h1>
 
         {isSearchVisible && (
@@ -242,3 +240,263 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Tasks);
+
+
+// import React, { useState, useEffect } from "react";
+// import {
+//   FiTrash2,
+//   FiXCircle,
+//   FiCheckCircle,
+//   FiEdit,
+//   FiList,
+//   FiSearch,
+// } from "react-icons/fi";
+// import { toast } from "react-toastify";
+// import Modal from "../../components/Modal";
+// import api from "../../services/api";
+// import { connect } from "react-redux";
+// import * as actions from "../../Store/actions/actionCreators";
+// import "./styles.css";
+
+// import AppBar from "@material-ui/core/AppBar";
+// import CssBaseline from "@material-ui/core/CssBaseline";
+// import Divider from "@material-ui/core/Divider";
+// import Drawer from "@material-ui/core/Drawer";
+// import Hidden from "@material-ui/core/Hidden";
+// import IconButton from "@material-ui/core/IconButton";
+// import List from "@material-ui/core/List";
+// import ListItem from "@material-ui/core/ListItem";
+// import ListItemIcon from "@material-ui/core/ListItemIcon";
+// import ListItemText from "@material-ui/core/ListItemText";
+// import MenuIcon from "@material-ui/icons/Menu";
+// import Toolbar from "@material-ui/core/Toolbar";
+// import Typography from "@material-ui/core/Typography";
+// import {
+//   makeStyles,
+//   useTheme,
+//   Theme,
+//   createStyles,
+// } from "@material-ui/core/styles";
+// import { Brightness4, Brightness7, Home, Info } from "@material-ui/icons";
+// import { Tooltip } from "@material-ui/core";
+
+// import { Box, Chip, Grid } from "@material-ui/core";
+// import { pink } from "@material-ui/core/colors";
+
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+// }));
+
+
+// function Tasks(props) {
+//   const [isModalVisible, setIsModalVisible] = useState(false);
+//   const [isSearchVisible, setIsSearchVisible] = useState(false);
+//   const [editId, setEditId] = useState(null);
+
+//   const total = props.todos.length;
+//   console.log(props.todos);
+//   const pending = props.todos.filter((conc) => conc.concluido === 0).length;
+//   const done = props.todos.filter((conc) => conc.concluido === 1).length;
+//   // const done = todos.filter((done) => done.concluido === 1).length;
+
+//   useEffect(() => {
+//     api.get("/tarefas").then((response) => {
+//       props.setTodos(response.data);
+//     });
+//   }, []);
+
+//   function handleEditTask(id) {
+//     setEditId(id);
+//     setIsModalVisible(true);
+//   }
+
+//   async function handleDeleteTask(id) {
+//     try {
+//       await api.delete(`/tarefas/${id}`);
+
+//       props.deleteTodo(id);
+//       toast.success("Excluído com sucesso!");
+//     } catch (err) {
+//       console.log(err);
+//       toast.error("Erro! Tente novamente.");
+//     }
+//   }
+
+//   return (
+//     <>
+//       {isModalVisible ? (
+//         <Modal
+//           id={editId}
+//           onClose={() => {
+//             setIsModalVisible(false);
+//             setEditId(null);
+//           }}
+//         />
+//       ) : null}
+
+//         <div className="task-container">
+//           <h1>
+//             <FiList className="list" size={28} color="#FF1493" />
+//             Lista de Tarefas
+//             <a
+//               className="icone-search"
+//               onClick={() => setIsSearchVisible(!isSearchVisible)}
+//             >
+//               <FiSearch size={24} color="#FF1493" />
+//             </a>
+//             <AppBar position="fixed">
+//               <Toolbar>
+//                 <IconButton
+//                   color="inherit"
+//                   aria-label="open drawer"
+//                   edge="start"
+//                 >
+//                   <MenuIcon />
+//                 </IconButton>
+//                 <div
+//                   style={{
+//                     display: "flex",
+//                     justifyContent: "space-between",
+//                     alignItems: "center",
+//                     flex: 1,
+//                   }}
+//                 >
+//                   <Typography variant="h6" noWrap>
+//                     Todo app
+//                   </Typography>
+
+//                   <IconButton onClick={props.changeTheme}>
+//                     <Tooltip arrow title={"Alterar entre tema escuro e claro"}>
+//                       {props.type === "dark" ? (
+//                         <Brightness7 />
+//                       ) : (
+//                         <Brightness4 />
+//                       )}
+//                     </Tooltip>
+//                   </IconButton>
+//                 </div>
+//               </Toolbar>
+//             </AppBar>
+//           </h1>
+
+//           {isSearchVisible && (
+//             <section>
+//               <input
+//                 className="search-input"
+//                 placeholder="Buscar..."
+//                 onChange={(e) => props.todoSearch(e.target.value)}
+//               />
+//             </section>
+//           )}
+
+//           <Box style={{ display: "inline" }}>
+//             <Box style={{ display: "inline" }} p={0.4}>
+//               <Chip
+//                 style={{ backgroundColor: pink[700], color: "white" }}
+//                 label={`Total: ${total}`}
+//               />
+//             </Box>
+
+//             <Box style={{ display: "inline" }} p={0.4}>
+//               <Chip
+//                 style={{ backgroundColor: pink[700], color: "white" }}
+//                 label={`Concluído: ${done}`}
+//               />
+//             </Box>
+
+//             <Box style={{ display: "inline" }} p={0.4}>
+//               <Chip
+//                 style={{ backgroundColor: pink[700], color: "white" }}
+//                 label={`Pendente: ${pending}`}
+//               />
+//             </Box>
+//           </Box>
+
+//             {props.todos.map((task) => (
+//                 <Grid item md={3} className="list-item" key={task.id}>
+//                   <strong>Título:</strong>
+//                   <p>{task.titulo}</p>
+
+//                   <strong>Descrição:</strong>
+//                   <p>{task.descricao}</p>
+
+//                   <strong>Concluído:</strong>
+//                   <p>
+//                     {task.concluido === 0 ? (
+//                       <FiXCircle size={18} color="#FF0000" />
+//                     ) : (
+//                       <FiCheckCircle size={18} color="#32CD32" />
+//                     )}
+//                   </p>
+
+//                   <button
+//                     className="edit"
+//                     onClick={() =>
+//                       handleEditTask(`${task.id}`, {
+//                         // id: task.id,
+//                         title: task.titulo,
+//                         description: task.descricao,
+//                         status: task.concluido,
+//                       })
+//                     }
+//                     // onClick={() =>
+//                     //   push(`/editar/${task.id}`, {
+//                     //     title: task.titulo,
+//                     //     description: task.descricao,
+//                     //     status: task.concluido,
+//                     //   })
+//                     // }
+//                   >
+//                     <FiEdit size={18} color="#FF1493" />
+//                   </button>
+
+//                   <button
+//                     onClick={() => handleDeleteTask(task.id)}
+//                     type="button"
+//                   >
+//                     <FiTrash2 size={18} color="#FF1493" />
+//                   </button>
+//             ))}
+
+//           <footer>
+//             <a className="button" onClick={() => setIsModalVisible(true)}>
+//               Nova tarefa
+//             </a>
+//           </footer>
+//         </div>
+//     </>
+//   );
+// }
+
+// function mapStateToProps(state) {
+//   console.log("mapStateToProps", state);
+//   return {
+//     todos: !state.todos.search
+//       ? state.todos.data
+//       : state.todos.data.filter((item) => {
+//           return (
+//             item.titulo.includes(state.todos.search) ||
+//             item.descricao.includes(state.todos.search)
+//           );
+//         }),
+//   };
+// }
+
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     setTodos(todos) {
+//       dispatch(actions.addTodo(todos));
+//     },
+//     deleteTodo(id) {
+//       dispatch(actions.deleteTodo(id));
+//     },
+//     todoSearch(value) {
+//       dispatch(actions.todoSearch(value));
+//     },
+//   };
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Tasks);
